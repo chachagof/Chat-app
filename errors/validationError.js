f0f0e3fccb94d0e92c92e025511be0c0eef51463
chachagof <chacha0932@gmail.com>
2024-05-02 21:09:09 +0800

    feat(errors): Add vslidation error class

---
+++ errors/validationError.js
@@ -0,0 +1,7 @@
+export default class ValidationError extends Error {
+  constructor(message, statusCode) {
+    super(message);
+    this.name = 'ValidationError';
+    this.statusCode = statusCode;
+  }
+}
