78711ec7badf288c210ef957c8bd21c432df611f
chachagof <chacha0932@gmail.com>
2024-05-03 10:53:53 +0800

    feat(api):add friendship api router

--- apis/index.js
+++ apis/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import userController from '../controllers/userController.js';
+import friendshipController from '../controllers/friendshipController.js';
 import passport from '../config/passport.js';
 import authenticated from '../middleware/authenticate.js';
 
@@ -18,5 +19,14 @@ router.post(
 // user info
 router.get('/users/:userId', authenticated, userController.getUser);
 router.put('/users/:userId', authenticated, userController.editUser);
+
+// get friendships
+router.get('/friendship', authenticated, friendshipController.getFriendship);
+
+// change friendship
+router.post('/friendship', authenticated, friendshipController.changeFriendship);
+
+// delete friendship
+router.delete('/friendship', authenticated, friendshipController.deleteFriendship);
 
 export default router;
